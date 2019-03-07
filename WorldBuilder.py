d312b76a13f8fb736650ac1911ae351fc3231d0a
Matt <MDSall7@gmail.com>
2019-03-07 05:46:56 -0600

    added code option for drawing

--- WorldBuilder.py
+++ WorldBuilder.py
@@ -1,14 +1,19 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 def BuildWorld(numCities):
     capidx = 0
 
     #randomly generated node web
-    web = nx.barabasi_albert_graph(numCities,2)
+    web = nx.barabasi_albert_graph(numCities,1)
 
     #finds city with most roads out, names it the capital
     for i in web.nodes:
+#        print("Roads out of " + str(i) + ": " + str(web.edges(i)) + str(len(web.edges(i))))
         if len(web.edges(i)) > len(web.edges(capidx)):
             capidx = i
 
-    return(web)
+    nx.draw(web)
+    plt.show()
+
+    return(web, capidx)

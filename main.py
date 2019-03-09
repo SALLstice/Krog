21b06cc93e1747777434373c7b51da564b10d4a2
Matt <MDSall7@gmail.com>
2019-03-08 23:00:00 -0600

    added debug menu, edited to fit with new item stuff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sequences import *
 from WorldBuilder import *
+from places import *
 
 name = input("Enter your name:")
 WorldSize = int(input("Number of Cities? "))
@@ -9,25 +10,25 @@ worldWeb, capital = buildWorld(WorldSize)
 location = 0
 currentHP = 10
 overlandSpeed = 3
-equip = [1,3]
-bag = [5,5]
+equip = [createItem(0,'Training Sword',0),createItem(0,'Clothing',0)]
+bag = [createItem(0,'Curative Herbs',0)]
 inv = [equip, bag, 0]
 
 while 1:
-  activity = int(input("\n1:Fight\n2:Inventory\n3:Location\n4:World\n"))
+  a = int(input("\n1:Fight\n2:Inventory\n3:Location\n4:World\n"))
   
-  if activity == 1:
+  if a == 1:
     result, currentHP, goldGain, xpGain = combat(name, currentHP, location, inv)
     if result == 1:
       inv[2] =+ goldGain
       inv[3] =+ xpGain
       result = 0 
-  elif activity == 2:
+  elif a == 2:
     inventory(inv)
-  elif activity == 3:
+  elif a == 3:
     print("Shop\nRumors\nInfo")
     locInfo(worldWeb, location)
-  elif activity == 4:
+  elif a == 4:
     x = int(input("1: World Info\n2: Travel"))
     if x == 1: worldInfo(worldWeb, capital)
     elif x == 2:
@@ -37,5 +38,14 @@ while 1:
         print("It took you " + '%.1f'%(int(worldWeb[location][travres]['length']) / overlandSpeed) + " hours.")
         location = travres
         print("You are now in " + str(location))
-        
-
+  elif a == 696969:
+    debug = int(input("\n Debug \n 1: every item type \n2: every item entity\n3: every site entity"))
+    if debug == 1:
+      for i in range(len(itemList)):
+        print(itemList[i].name)
+    elif debug == 2:
+        for i in range(len(itemEntityList)):
+          print(itemEntityList[i].entityID,itemEntityList[i].name, itemEntityList[i].itemType, itemEntityList[i].desc)
+    elif debug == 3:
+        for i in range(len(siteEntityList)):
+          print(siteEntityList[i].entityID,siteEntityList[i].name, siteEntityList[i].siteType, siteEntityList[i].loc, siteEntityList[i].civil)

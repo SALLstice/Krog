ab472aab517f6a305cf71fc002b0642103feccbe
Matt <MDSall7@gmail.com>
2019-03-08 22:59:29 -0600

    each site has unique ID and attributes

--- places.py
+++ places.py
@@ -1,10 +1,40 @@
 import csv
+from items import *
 
-#itemID; Name; itemType; itemStrength, itemWorth
+siteList = []
+siteEntityList = []
 
-with open(r"C:\Users\Matt\Documents\GitHub\Krog\buildings.txt") as f:
-  reader = csv.reader(f)
-  buildingList = tuple(reader)
+class site:
+    def __init__(self,name,siteType):
+        self.name = name
+        self.siteType = siteType
 
-def checkBuilding(buildingID):
-    return(buildingList[buildingID][1])
+class siteEntity:
+    def __init__(self,entityID,name,siteType,loc,civil,stock):
+        self.entityID = entityID
+        self.name = name
+        self.siteType = siteType
+        self.loc = loc
+        self.civil = civil
+        self.stock = stock
+
+###create siteList from file
+##with open(r'C:\Users\Matt\Documents\GitHub\Krog\sites.txt', 'r') as f:
+##    reader = csv.reader(f)
+##    for row in reader:
+##        siteList.append(site(row[0], row[1]))
+
+def createSite(name,siteType,loc,civil,stock):
+    siteEntityList.append(siteEntity(int(len(siteEntityList)),name,siteType,loc,civil,stock))
+    return siteEntityList[int(len(siteEntityList)-1)].entityID
+
+def locInfo(web, loc):
+    print("\nYou are in " + str(loc) + ".")
+    for x in range(len(web.nodes[loc]['sites'])):
+        print(siteEntityList[web.nodes[loc]['sites'][x]].name, siteEntityList[web.nodes[loc]['sites'][x]].siteType,siteEntityList[web.nodes[loc]['sites'][x]].entityID)
+    shop(int(input()))
+
+def shop(entityID):
+    print("You are at " + str(siteEntityList[entityID].name))
+    print("Stock:")
+    print(itemEntityList[siteEntityList[entityID].stock].itemType)
